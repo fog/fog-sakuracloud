1675c0d44f33cc000016dd61f6073ed86072d71e
sawanoboly <sawanoboriyu@higanworks.com>
2014-08-31 15:29:25 +0900

    swap helpers

--- tests/helper.rb
+++ tests/helper.rb
@@ -1,7 +1,8 @@
+## From fog-core
+require 'fog/test_helpers/formats_helper'
+
 ## From fog submodule
 require File.expand_path('../../fog/tests/helper', __FILE__)
 helpers = Dir.glob(File.expand_path('../../', __FILE__) + '/fog/tests/helpers/**/*.rb')
 helpers.map {|h| load h}
 
-## From fog-core
-require 'fog/test_helpers/formats_helper'
