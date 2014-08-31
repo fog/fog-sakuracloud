c80860cac83077836023bf4f2d00018662d2027a
sawanoboly <sawanoboriyu@higanworks.com>
2014-08-31 15:12:31 +0900

    require fog/schema/data_validator for travis

--- tests/helper.rb
+++ tests/helper.rb
@@ -1,3 +1,4 @@
+require 'fog/schema/data_validator'
 require File.expand_path('../../fog/tests/helper', __FILE__)
 helpers = Dir.glob(File.expand_path('../../', __FILE__) + '/fog/tests/helpers/**/*.rb')
 helpers.map {|h| load h}
