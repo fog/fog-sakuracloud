72025345f05b79e31db2bc6827ede9d3bb80a7c9
sawanoboly <sawanoboriyu@higanworks.com>
2015-12-08 20:35:59 +0900

    show Sizes of diskplan

--- lib/fog/sakuracloud/models/volume/plan.rb
+++ lib/fog/sakuracloud/models/volume/plan.rb
@@ -6,6 +6,7 @@ module Fog
       class Plan < Fog::Model
         identity :id, :aliases => 'ID'
         attribute :name, :aliases => 'Name'
+        attribute :size, :aliases => 'Size'
       end
     end
   end
